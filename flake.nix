fa2710c12d5806b8891f5c17190b27649e18f5b2
Floryan Simar <floryansimar@gmail.com>
2024-11-22 10:28:35 +0100

    chore: added nix flake for development environment

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A basic gomod2nix flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gomod2nix = {
+    url = "github:nix-community/gomod2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    gomod2nix,
+  }: (
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      # The current default sdk for macOS fails to compile go projects, so we use a newer one for now.
+      # This has no effect on other platforms.
+      callPackage = pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
+    in {
+      formatter = pkgs.alejandra;
+      devShells.default = callPackage ./shell.nix {
+        inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
+      };
+    })
+  );
+}
